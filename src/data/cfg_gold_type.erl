b8333942072b72d66b9e238f09b0872dd629ed6f
guli <guli@tsinghua.org.cn>
2018-05-04 20:39:00 +0800

    base:

--- src/data/cfg_gold_type.erl
+++ src/data/cfg_gold_type.erl
@@ -9,13 +9,37 @@
 -include("record_cfg_gold_type.hrl").
 
 get_ids() ->
-	[<<"BGX">>].
+	[<<"BGX">>,<<"BTC">>,<<"ELA">>,<<"ETH">>].
 
 get(<<"BGX">>) ->
 	#gold_type{
 		gold_type = <<"BGX"/utf8>>,
 		mining_start_time = 1525017600,
 		mining_output_first_day = 1027397.26,
+		half_life_days = 730
+	};
+
+get(<<"BTC">>) ->
+	#gold_type{
+		gold_type = <<"BTC"/utf8>>,
+		mining_start_time = 1525017600,
+		mining_output_first_day = 1.0e4,
+		half_life_days = 730
+	};
+
+get(<<"ELA">>) ->
+	#gold_type{
+		gold_type = <<"ELA"/utf8>>,
+		mining_start_time = 1525017600,
+		mining_output_first_day = 1.0e5,
+		half_life_days = 730
+	};
+
+get(<<"ETH">>) ->
+	#gold_type{
+		gold_type = <<"ETH"/utf8>>,
+		mining_start_time = 1525017600,
+		mining_output_first_day = 1.0e5,
 		half_life_days = 730
 	};
 
