660d7ebf2d808c9f52fc0f2e05fe4f62439200b8
guli <guli@tsinghua.org.cn>
2018-06-22 17:57:00 +0800

    rpc refactor

--- src/lib/lib_rpc.erl
+++ src/lib/lib_rpc.erl
@@ -8,35 +8,26 @@
 -include("common.hrl").
 -include("record.hrl").
 
-rpc(?SVRTYPE_GAME, M, F, A) ->
+rpc(SvrType, M, F, A) ->
+    Server = get_svr(SvrType),
+    case rpc:call(Server#server.node, M, F, A) of
+        {badrpc, Reason} ->
+            ?ERR("rpc exception:~nerr_msg=~p~n", [Reason]),
+            throw({?ERRNO_EXCEPTION, ?T2B(Reason)});
+        {ErrNo, ErrMsg} when is_integer(ErrNo), is_binary(ErrMsg) ->
+            throw({ErrNo, ErrMsg});
+        Ret -> Ret
+    end.
+
+get_svr(?SVRTYPE_GAME) ->
     case mod_disperse:gamesvr_list() of
-        [] ->
-            throw({?ERRNO_NO_RPC_SERVER, <<"no game server">>});
-        Servers ->
-            Server = get_rand_svr(Servers),
-            case rpc:call(Server#server.node, M, F, A) of
-                {badrpc, Reason} ->
-                    ?ERR("rpc exception:~nerr_msg=~p~n", [Reason]),
-                    throw({?ERRNO_EXCEPTION, ?T2B(Reason)});
-                {ErrNo, ErrMsg} when is_integer(ErrNo), is_binary(ErrMsg) ->
-                    throw({ErrNo, ErrMsg});
-                Ret -> Ret
-            end
+        [] -> throw({?ERRNO_NO_RPC_SERVER, <<"no game server">>});
+        Servers -> get_rand_svr(Servers)
     end;
-rpc(?SVRTYPE_XCHG, M, F, A) ->
+get_svr(?SVRTYPE_XCHG) ->
     case mod_disperse:xchgsvr_list() of
-        [] ->
-            throw({?ERRNO_NO_RPC_SERVER, <<"no xchg server">>});
-        Servers ->
-            Server = get_rand_svr(Servers),
-            case rpc:call(Server#server.node, M, F, A) of
-                {badrpc, Reason} ->
-                    ?ERR("rpc exception:~nerr_msg=~p~n", [Reason]),
-                    throw({?ERRNO_EXCEPTION, ?T2B(Reason)});
-                {ErrNo, ErrMsg} when is_integer(ErrNo), is_binary(ErrMsg) ->
-                    throw({ErrNo, ErrMsg});
-                Ret -> Ret
-            end
+        [] -> throw({?ERRNO_NO_RPC_SERVER, <<"no xchg server">>});
+        Servers -> get_rand_svr(Servers)
     end.
 
 get_rand_svr([Server]) ->
