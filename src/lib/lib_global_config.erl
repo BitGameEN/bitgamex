151ba70a481615379f9648581a5d459b503abe19
guli <guli@tsinghua.org.cn>
2018-11-01 18:47:47 +0800

    util:binary_to_float

--- src/lib/lib_global_config.erl
+++ src/lib/lib_global_config.erl
@@ -14,31 +14,31 @@ get(?GLOBAL_CONFIG_KEY_TRANSFER_DISCOUNT_IN_GAME = Key) ->
     case usr_global_config:get_one(Key) of
         [] -> 0;
         #usr_global_config{content = Val} ->
-            util:zero_if_negative(binary_to_float(Val))
+            util:zero_if_negative(util:binary_to_float(Val))
     end;
 get(?GLOBAL_CONFIG_KEY_TRANSFER_DISCOUNT_TO_XCHG = Key) ->
     case usr_global_config:get_one(Key) of
         [] -> 0;
         #usr_global_config{content = Val} ->
-            util:zero_if_negative(binary_to_float(Val))
+            util:zero_if_negative(util:binary_to_float(Val))
     end;
 get(?GLOBAL_CONFIG_KEY_GOLD_PROPORTION_FOR_LOGIN = Key) ->
     case usr_global_config:get_one(Key) of
         [] -> 0.3;
         #usr_global_config{content = Val} ->
-            util:clamp(0, 1, binary_to_float(Val))
+            util:clamp(0, 1, util:binary_to_float(Val))
     end;
 get(?GLOBAL_CONFIG_KEY_GOLD_PROPORTION_FOR_RANDOM = Key) ->
     case usr_global_config:get_one(Key) of
         [] -> 0.1;
         #usr_global_config{content = Val} ->
-            util:clamp(0, 1, binary_to_float(Val))
+            util:clamp(0, 1, util:binary_to_float(Val))
     end;
 get(?GLOBAL_CONFIG_KEY_GOLD_PROPORTION_FOR_GAME = Key) ->
     case usr_global_config:get_one(Key) of
         [] -> 0.6;
         #usr_global_config{content = Val} ->
-            util:clamp(0, 1, binary_to_float(Val))
+            util:clamp(0, 1, util:binary_to_float(Val))
     end;
 get(_) ->
     undefined.
